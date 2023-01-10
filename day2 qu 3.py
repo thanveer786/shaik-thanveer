aecca9c36775b923ee1a899f4144dc3d70c9c3aa
thanveer786 <122257102+thanveer786@users.noreply.github.com>
2023-01-10 20:07:11 +0530

    Add files via upload

---
+++ day2 qu 3.py
@@ -0,0 +1,6 @@
+count0fwords = len("Hello This is python programming".split())
+print("Count of Words in given sentence:", count0fwords)
+
+print(len("Hello This is python programming".split()))
+
+print(len(input("Enter Input:").split())) 
